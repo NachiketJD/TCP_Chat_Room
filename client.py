1dea0084dc229c208a89568057e98f58b56d6a92
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-12 20:12:53 +0530

    Client_Rec Fn

--- client.py
+++ client.py
@@ -4,3 +4,15 @@ import socket
 alias = input('Chhose an alias>>')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 59000))
+
+def client_recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')# 1024 is the buffer size
+            if message == 'alias?':
+                client.send(alias.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print('An error occurred')
+            break
